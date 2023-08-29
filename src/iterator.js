181340b1ab3ba09d79b7e16519cf19e37275cf58
Xenia Ch <eradonna@yandex.ru>
2023-08-29 21:40:03 +0300

    array creation redone

--- src/iterator.js
+++ src/iterator.js
@@ -3,7 +3,7 @@ export default class Team {
     this.size = size;
   }
 
-  static fill() {
+  fill() {
     const names = ['Lisette', 'Morren', 'Fafnir', 'Constance', 'Eustacia'];
     const types = ['Bowman', 'Swordsman', 'Magician'];
     const health = 100;
@@ -11,27 +11,29 @@ export default class Team {
     const attacks = [25, 10, 25, 40, 30];
     const defences = [25, 10, 25, 40, 30];
 
-    const name = names[Math.floor(Math.random() * names.length)];
-    const type = types[Math.floor(Math.random() * types.length)];
-    const attack = attacks[Math.floor(Math.random() * attacks.length)];
-    const defence = defences[Math.floor(Math.random() * defences.length)];
-    const value = {
-      name, type, health, level, attack, defence,
-    };
-    return value;
+    const members = [];
+    for (let i = 0; i < this.size; i += 1) {
+      const name = names[Math.floor(Math.random() * names.length)];
+      const type = types[Math.floor(Math.random() * types.length)];
+      const attack = attacks[Math.floor(Math.random() * attacks.length)];
+      const defence = defences[Math.floor(Math.random() * defences.length)];
+      const value = {
+        name, type, health, level, attack, defence,
+      };
+      members.push(value);
+    }
+    return members;
   }
 
   [Symbol.iterator]() {
     let index = 0;
-    const members = [];
 
     return {
       next: () => {
         if (index < this.size) {
           index += 1;
-          members.push(Team.fill());
           return {
-            value: members[index - 1],
+            value: this.fill()[index - 1],
             done: false,
           };
         }
